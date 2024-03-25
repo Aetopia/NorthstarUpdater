1dbc99b9729b75c2e1359c0ef7c74b2718341545
Aetopia <kirbymario22@gmail.com>
2024-03-25 22:52:05 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Web.Script.Serialization;
+using System.Collections;
+using System.IO;
+using System.Security.Cryptography;
+using System.IO.Compression;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    [DllImport("User32")]
+    [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
+    private static extern IntPtr FindWindowW([MarshalAs(UnmanagedType.LPWStr)] string lpClassName, [MarshalAs(UnmanagedType.LPWStr)] string lpWindowName);
+
+    readonly static MD5 md5 = MD5.Create();
+
+    private static string GetMD5Hash(string path)
+    {
+        try { return Convert.ToBase64String(md5.ComputeHash(File.ReadAllBytes(path))); }
+        catch { return default; }
+    }
+
+    private static string GetMD5Hash(Stream stream)
+    {
+        return Convert.ToBase64String(md5.ComputeHash(stream));
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("[INFO] Northstar Updater by Aetopia.");
+        Console.WriteLine("[INFO] GitHub Repository: https://github.com/Aetopia/Northstar-Updater");
+        Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+
+        using WebClient webClient = new();
+        webClient.Headers.Add(HttpRequestHeader.UserAgent, "_");
+
+        try
+        {
+            Console.WriteLine("[INFO] Obtain latest release information.");
+            string input = webClient.DownloadString("https://api.github.com/repos/R2Northstar/Northstar/releases/latest");
+            string requestUriString = ((new JavaScriptSerializer().Deserialize<Dictionary<string, object>>(input)["assets"] as ArrayList)[0] as Dictionary<string, object>)["browser_download_url"] as string;
+
+            WebRequest webRequest = WebRequest.Create(requestUriString);
+            webRequest.Method = "HEAD";
+
+            using WebResponse webResponse = webRequest.GetResponse();
+            if (!File.Exists("Northstar.zip") || GetMD5Hash("Northstar.zip") != webResponse.Headers["Content-MD5"])
+            {
+                Console.WriteLine("[INFO] Couldn't validate cached release.");
+                try { webClient.DownloadFileTaskAsync(requestUriString, "Northstar.zip").GetAwaiter().GetResult(); }
+                catch (Exception e) { Console.WriteLine($"[ERROR] {e.Message}"); }
+            }
+            else
+                Console.WriteLine("[INFO] Validated cached release.");
+        }
+        catch (Exception e) { Console.WriteLine($"[ERROR] {e.Message}"); }
+
+        try
+        {
+            using ZipArchive zipFile = ZipFile.OpenRead("Northstar.zip");
+            for (int i = 0; i < zipFile.Entries.Count; i++)
+            {
+                if (zipFile.Entries[i].Length == 0)
+                    Directory.CreateDirectory(zipFile.Entries[i].FullName);
+                else
+                {
+                    using Stream stream = zipFile.Entries[i].Open();
+                    if (GetMD5Hash(stream) != (File.Exists(zipFile.Entries[i].FullName) ? GetMD5Hash(zipFile.Entries[i].FullName) : default))
+                    {
+                        Console.WriteLine($"[INFO] {zipFile.Entries[i].FullName} : Extract");
+                        zipFile.Entries[i].ExtractToFile(@$".\\{zipFile.Entries[i].FullName}", true);
+                    }
+                    else Console.WriteLine($"[INFO] {zipFile.Entries[i].FullName} : Pass");
+                }
+            }
+        }
+        catch (Exception e) { Console.WriteLine($"[ERROR] {e.Message}"); }
+
+        ProcessStartInfo startInfo = new()
+        {
+            FileName = "NorthstarLauncher.exe",
+            CreateNoWindow = true,
+            UseShellExecute = false
+        };
+        try
+        {
+            string executableFileName = Environment.GetCommandLineArgs()[0];
+            startInfo.Arguments = Environment.CommandLine.Remove(Environment.CommandLine.IndexOf(executableFileName), executableFileName.Length).TrimStart('"').Substring(1);
+        }
+        catch { }
+        try { Process.Start(startInfo).Dispose(); }
+        catch (Exception e) { Console.WriteLine(e); }
+    }
+}
---
+++ README.md
@@ -0,0 +1,107 @@
+> [!IMPORTANT]
+> This repository is not with [Northstar](https://github.com/R2Northstar)!
+
+# Northstar Updater
+A dedicated standalone updater & installer for [Northstar](https://github.com/R2Northstar), a Titanfall 2 modding and custom server framework.
+
+> [!NOTE]
+> If you need more robust functionality please consider looking into:<br>
+> - [FlightCore](https://github.com/R2NorthstarTools/FlightCore)
+> - [VTOL](https://github.com/BigSpice/VTOL)
+> - [Viper](https://github.com/0neGal/viper)
+
+## Features
+The Northstar Updater provides the following features:
+- Updating/Installing Northstar.
+- Launching Northstar.
+- Automatic Updates for Northstar.
+
+> [!NOTE]
+> The updater only manages the [Northstar Launcher](https://github.com/R2Northstar/NorthstarLauncher).
+
+## Installation
+1. Download the latest version of Northstar Updater from [GitHub Releases](https://github.com/Aetopia/Northstar-Updater/releases).
+
+2. Place `NorthstarUpdater.exe` in the root of Titanfall 2's installation directory.
+    |Store|Default Location|
+    |-|-|
+    |Steam|`C:\Program Files (x86)\Steam\steamapps\common\Titanfall2`|
+    |Origin|`C:\Program Files (x86)\Origin Games\Titanfall2`|
+    |EA App|`C:\Program Files\EA Games\Titanfall2`|
+
+3. Run `NorthstarUpdater.exe` to install and run Northstar.
+    > Any arguments passed to `NorthstarUpdater.exe` will forwarded to Northstar.
+
+## Uninstallation
+### Steam
+Run the following script in PowerShell.
+
+```powershell
+Get-Content  "$(Split-Path $(([string]((Get-ItemPropertyValue `
+-Path "Registry::HKEY_CLASSES_ROOT\steam\Shell\Open\Command" -Name "(Default)") `
+-Split "-", 2, "SimpleMatch")[0]).Trim().Trim('"')))\config\libraryfolders.vdf" | 
+ForEach-Object { 
+    if ($_ -like '*"path"*') {
+        [string]$Path = "$(([string]$_).Trim().Trim('"path"').Trim().Trim('"').Replace("\\", "\"))\steamapps\common\Titanfall2" 
+        @(
+            "bin", 
+            "R2Northstar", 
+            "debug.log", 
+            "LEGAL.txt", 
+            "Northstar.dll", 
+            "Northstar.zip", 
+            "NorthstarLauncher.exe", 
+            "NorthstarUpdater.exe", 
+            "ns_startup_args.txt", 
+            "ns_startup_args_dedi.txt", 
+            "r2ds.bat"
+        ) | 
+        ForEach-Object {
+            try { Remove-Item "$Path\$_" -Recurse -Force }
+            catch {}
+        }
+    }
+}
+```
+
+### Origin/EA App
+Run the following script in PowerShell in the root of Titanfall 2's installation directory.
+
+```powershell
+@(
+    "bin", 
+    "R2Northstar", 
+    "debug.log", 
+    "LEGAL.txt", 
+    "Northstar.dll", 
+    "Northstar.zip", 
+    "NorthstarLauncher.exe", 
+    "NorthstarUpdater.exe", 
+    "ns_startup_args.txt", 
+    "ns_startup_args_dedi.txt", 
+    "r2ds.bat"
+) | 
+ForEach-Object {
+    try { Remove-Item "$Path\$_" -Recurse -Force }
+    catch {}
+}
+```
+
+### Manual
+```
+bin
+R2Northstar
+debug.log
+LEGAL.txt 
+Northstar.dll 
+Northstar.zip
+NorthstarLauncher.exe
+NorthstarUpdater.exe
+ns_startup_args.txt 
+ns_startup_args_dedi.txt
+r2ds.bat
+```
+Delete the following files/folders from Titanfall 2's installation directory.
+
+> [!IMPORTANT]
+> Once you uninstall Northstar, make sure to repair the game via Steam, Origin or the EA App.
